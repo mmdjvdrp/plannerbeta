8620cace5717809c9509d1122877de71f6a55df9
mmdjvdrp <mohammadjrahimpour@gmail.com>
2026-06-17 09:47:39 +0330

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,7 @@
 import { initializeApp }
+  import { initializeApp } from "./firebase-app.js";
+import { getAuth, ... } from "./firebase-auth.js";
+import { getDatabase, ... } from "./firebase-database.js";
 from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 
 import {
