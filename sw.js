9dcc39bc44d1cc5b08f4df148890877f32ec4f15
mmdjvdrp <mohammadjrahimpour@gmail.com>
2026-06-18 11:13:25 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,6 @@ const assetsToCache = [
   "/js/app.js",
   "/js/event.js",
   "/js/supabase.js",
-  "/js/planner.js", // ثبت اسکریپت جدید برای استفاده آفلاین
   "/js/storage.js",
   "/js/ui.js",
   "/icons/icon-192.png",
